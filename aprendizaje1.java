ee3025686249a6bb7c8e045469bc8e0fd3b987df
SaraguroKatherine <ksaraguro6087@utm.edu.ec>
2023-10-26 20:02:59 -0500

    Determinar el tipo de operación aritmética que se va a realizar en función de la entrada del usuario

--- aprendizaje1.java
+++ aprendizaje1.java
@@ -13,6 +13,38 @@ public class aprendizaje1 {
         nivel = input.nextInt();
         System.out.print("Introduzca el tipo de operación aritmética (1-4): ");
         tipo = input.nextInt();
-     }
+         // Generar problemas aritméticos aleatorios basados en la entrada del usuario
 
+         for (int i = 1; i <= 10; i++) {
+            int num1, num2, respuesta, resultado;
+            String operador = "";
+
+            // Genere números aleatorios basados en la entrada del usuario
+
+            if (nivel == 1) {
+                num1 = random.nextInt(10);
+                num2 = random.nextInt(10);
+            } else if (nivel == 2) {
+                num1 = random.nextInt(90) + 10;
+                num2 = random.nextInt(90) + 10;
+            } else {
+                num1 = random.nextInt(900) + 100;
+                num2 = random.nextInt(900) + 100;
+            } 
+            // Determinar el tipo de operación aritmética que se va a realizar en función de la entrada del usuario
+
+            if (tipo == 1) {
+                operador = "+";
+                resultado = num1 + num2;
+            } else if (tipo == 2) {
+                operador = "-";
+                resultado = num1 - num2;
+            } else if (tipo == 3) {
+                operador = "*";
+                resultado = num1 * num2;
+            } else {
+                operador = "/";
+                resultado = num1 / num2;
+            }
+        }
     }
