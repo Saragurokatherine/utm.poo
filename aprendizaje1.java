cafd822102fe7399ba6f249bf8a1268fded64ddf
SaraguroKatherine <ksaraguro6087@utm.edu.ec>
2023-10-26 19:11:18 -0500

    Pida al usuario que elija el nivel de dificultad y el tipo de operación aritmética a estudiar

---
+++ aprendizaje1.java
@@ -0,0 +1,17 @@
+import  java.util.Scanner;
+import  java.security.SecureRandom;
+
+public class aprendizaje1 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        SecureRandom random = new SecureRandom();
+        int nivel, tipo, correcto = 0, incorrecto = 0;
+
+        // Pida al usuario que elija el nivel de dificultad y el tipo de operación aritmética a estudiar
+
+        System.out.print("Introduce el nivel de dificultad (1-3): ");
+        nivel = input.nextInt();
+        System.out.print("Introduzca el tipo de operación aritmética (1-4): ");
+        tipo = input.nextInt();
+        
+    }
